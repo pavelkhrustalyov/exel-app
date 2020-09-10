c83a69ca304e8ca6b7855ebffcfcfe6d5f425308
pavel_khrustalyov <pavel_khrustalyov@mail.ru>
2020-09-10 19:26:18 +0300

    table-logic-finish

--- src/components/formula/Formula.js
+++ src/components/formula/Formula.js
@@ -3,21 +3,38 @@ import {ExcelComponent} from '@core/ExcelComponent';
 export class Formula extends ExcelComponent {
   static className = 'excel__formula';
 
-  constructor($root) {
+  constructor($root, options) {
     super($root, {
       name: 'Formula',
-      listeners: ['input'],
+      listeners: ['input', 'keydown'],
+        ...options,
+    });
+  }
+
+  onInput(event) {
+    this.text = event.target.textContent.trim();
+    this.$emit('formula:input', this.text);
+  }
+
+  onKeydown(event) {
+      if (event.key === 'Enter' || event.key === 'Tab') {
+        event.preventDefault();
+        this.$emit('formula:done');
+      }
+  }
+
+  init() {
+    super.init();
+    this.$formula = this.$root.find('#formula');
+    this.$on('table:select', ($cell) => {
+      this.$formula.text = $cell.text;
     });
   }
 
   toHTML() {
     return `
       <div class="info">fx</div>
-      <div class="input" contenteditable spellcheck="false"></div>
+      <div id="formula" class="input" contenteditable spellcheck="false"></div>
     `;
-  }
-
-  onInput(event) {
-    console.log('onInput formula ', event.target.textContent.trim());
   }
 }
--- src/core/utils.js
+++ src/core/utils.js
@@ -4,3 +4,12 @@ export const capitalize = (str) => {
   }
   return `on${str[0].toUpperCase()}${str.slice(1)}`;
 };
+
+export const range = (start, end) => {
+  if (start > end) {
+    [end, start] = [start, end];
+  }
+  return new Array(end - start + 1)
+      .fill('')
+      .map((_, index) => start + index);
+};
